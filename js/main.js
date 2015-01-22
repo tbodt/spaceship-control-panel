c844135dea2a074393dcd3120b9eb0c9fcf930a9
Theodore Dubois <tblodt@icloud.com>
2015-01-21 16:04:32 -0800

    try and make JS work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+var $buttons = $(".control-panel");
 
+var extraButtons = [
+    "Self-Destruct",
+    "Self-Destruct and Regret It",
+    "Activate AI",
+    "Deactivate AI",
+    ""
+];
+var extraButtonsIndex = 0;
+
+$("#add").click(function() {
+    if (extraButtonsIndex < extraButtons.length) {
+        $buttons.append($("<button type=\"button\">" + extraButtons[extraButtonsIndex] + "</button>"));
+        extraButtonsIndex++;
+    }
+});
