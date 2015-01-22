13f5b255f01a79662b7ae18d24906eed6020afb8
Theodore Dubois <tblodt@icloud.com>
2015-01-21 16:27:30 -0800

    add nice javascripty things

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-var $buttons = $(".control-panel");
-
 var extraButtons = [
     "Self-Destruct",
     "Self-Destruct and Regret It",
     "Activate AI",
     "Deactivate AI",
-    ""
+    "Roll Credits"
 ];
 var extraButtonsIndex = 0;
 
-$("#add").click(function() {
-    if (extraButtonsIndex < extraButtons.length) {
-        $buttons.append($("<button type=\"button\">" + extraButtons[extraButtonsIndex] + "</button>"));
-        extraButtonsIndex++;
-    }
+$(document).ready(function () {
+    var $buttons = $(".control-panel");
+    
+    $("#add").click(function () {
+        if (extraButtonsIndex < extraButtons.length) {
+            $buttons.append($("<button type=\"button\">" + extraButtons[extraButtonsIndex] + "</button>"));
+            extraButtonsIndex++;
+        }
+    });
 });
