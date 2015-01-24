b431e0c2059421a612137ef2ee40d04da1b23066
Theodore Dubois <tblodt@icloud.com>
2015-01-23 17:24:33 -0800

    add throttle

--- js/main.js
+++ js/main.js
@@ -10,12 +10,20 @@ var extraButtons = [
 var extraButtonsIndex = 0;
 
 $(document).ready(function () {
-    var $buttons = $(".control-panel");
+    var $buttons = $(".buttons");
     
     $("#add").click(function () {
         if (extraButtonsIndex < extraButtons.length) {
             $buttons.append($("<button type=\"button\">" + extraButtons[extraButtonsIndex] + "</button>"));
             extraButtonsIndex++;
         }
+    });
+    
+    $('input[type="range"]').rangeslider({
+        polyfill: false,
+        
+        rangeClass: "slider",
+        fillClass: "slider-fill",
+        handleClass: "slider-handle"
     });
 });
